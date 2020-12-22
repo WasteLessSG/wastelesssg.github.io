10d85a7007e6e6becefaae4a13fef8c80eef8f1d
Anthony Ardi Sukotjo <sukotjo33@yahoo.com.sg>
2020-12-23 05:27:40 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.png';
+import './components/navbar';
+import './App.css';
+import NavigationBar from "./components/navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavigationBar></NavigationBar>
+        <body>
+        Hello World
+        </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,32 @@
+import logo from './../assets/logo.png';
+import React from "react";
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import styled from 'styled-components';
+
+
+const NavigationBar = () => {
+    return  (
+                <Navbar collapseOnSelect expand="lg" bg="light" variant="light"  sticky="top">
+                    <Nav className="mr-auto">
+                        <a className="navbar-brand" href="/home">
+                            <img src={logo} width={30} height={30} alt = ""  className="d-inline-block mr-1"/>
+                              <span>WasteLess</span>
+                        </a>
+                    </Nav>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link href="/features">About Us</Nav.Link>
+                            <Nav.Link href="/team">Our Team</Nav.Link>
+                            <NavDropdown title="Our Solution"  href="/solution" id="collapsible-nav-dropdown">
+                                <NavDropdown.Item href="/solution#app">App</NavDropdown.Item>
+                                <NavDropdown.Item href="/solution#hardware">Hardware</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="/contact">Contact Us</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+    );
+}
+
+export default NavigationBar;
