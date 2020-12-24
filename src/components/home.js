c352174ce9e7b1f59b2eecc3ea7bb02b3b2f0dde
pmr99 <pradeep991004@gmail.com>
2020-12-24 22:27:46 +0800

    Update Home Test
    Seeing if my change is reflected here

--- src/components/home.js
+++ src/components/home.js
@@ -3,8 +3,10 @@ import React from "react";
 const aboutUs = () => {
 
     return (
-        <h1>Home</h1>
-        <h2>im testing if this change is reflected in website</h2>
+      <container>
+        <h1>Home Page</h1>
+        <text>im testing if this change is reflected in website</text>
+      <container>
 
     );
 
