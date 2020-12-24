50a7a6a17e182227202d50b4257fa323186d3256
pmr99 <pradeep991004@gmail.com>
2020-12-25 00:48:54 +0800

    Updates Our Solutions 1

--- src/components/ourSolution.js
+++ src/components/ourSolution.js
@@ -5,15 +5,17 @@ const ourSolution = () => {
     return (
         <container>
         <h1>Our Solution</h1>
-            <text> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor quam eu posuere luctus. Quisque metus leo, consequat at euismod maximus, facilisis ut sem. Duis tincidunt felis efficitur lacus volutpat aliquet. Morbi suscipit, quam non commodo tempus, magna libero tincidunt mauris, nec tincidunt risus est in eros. Suspendisse iaculis laoreet magna id volutpat. Fusce eget nisl enim. Cras porttitor vehicula accumsan. Suspendisse velit purus, ultricies at risus ac, porttitor dapibus ex. Maecenas vel enim egestas, malesuada ligula eu, blandit mi. Phasellus tempus nisi eget erat mollis finibus. Vivamus a faucibus elit. In efficitur eget dui in placerat. Integer vestibulum dui in tortor tristique condimentum.
+          <h2>A CLEANER, GREENER SINGAPORE</h2>
+            <text> WasteLess promotes green habits and provides an avenue for Singaporeans to take measurable steps in safeguarding our environment.</text>
+          <h2>TRACK WASTAGE</h2>
+            <text> Monitor personal waste data and trends on an accessible, convenient platform wherever you are.</text>
+          <h2>CONNECT COMMUNITIES</h2>
+            <text> Compete with neighbours and the nation toward the goal of zero waste.</text>
+          <h2>INFORM PUBLIC WASTE MANAGEMENT</h2>
+            <text> Back up policy decisions with realtime data of waste production from a district to an individual level.</text>
+          <h2>WHAT IS BEHAVIOURAL PSYCHOLOGY?</h2>
+            <text> Behavioural psychology or behaviourism, is a theory suggesting that the environment shapes human behaviour. In a most basic sense, behavioural psychology is the study and analysis of observable behaviour. This field of psychology influenced thought heavily throughout the middle of the 20th century.</text>
 
-                Etiam ultricies lectus eu condimentum sodales. Mauris semper tincidunt urna, eu semper eros auctor ac. Nunc vitae nisi rutrum lectus varius euismod. Nullam id iaculis odio. Suspendisse feugiat sollicitudin dolor, quis mattis purus vestibulum et. Duis blandit tellus turpis, a egestas arcu ullamcorper in. Nam gravida leo in eros rhoncus aliquam. Morbi porttitor urna non placerat posuere. Curabitur elementum consequat molestie. Nunc posuere mi et dui faucibus, in hendrerit ligula viverra.
-
-                Pellentesque tristique maximus facilisis. Maecenas varius mi non egestas vulputate. Suspendisse consequat placerat arcu, non finibus justo rutrum quis. Duis id congue ipsum. Curabitur in neque tempus, iaculis metus eu, rhoncus ligula. Sed mi nisi, ultricies finibus vulputate non, convallis eget ipsum. Nam fringilla, tortor id ullamcorper tincidunt, mauris velit laoreet nisl, vel commodo velit enim ac mauris. Fusce placerat nec enim quis vestibulum. Donec luctus sed diam sit amet vestibulum. Sed aliquet, orci nec hendrerit volutpat, mi lorem semper eros, eu sodales nisi ligula a nisi. Cras a ullamcorper ante.
-
-                Nullam non tincidunt odio, sit amet suscipit ex. Integer quis elit velit. Phasellus vitae mattis ex. In a tortor rutrum, rhoncus tortor ut, efficitur quam. Nam ut magna sed quam cursus consectetur. Praesent sit amet volutpat est. Aliquam hendrerit mauris vitae metus hendrerit pharetra. Integer quis mi ac urna sodales mattis id quis nibh. Etiam risus ligula, condimentum sit amet lorem in, egestas sagittis elit. Sed euismod viverra lorem, eget placerat tortor finibus vitae. In ac sem est. Nunc ut felis in velit tempor faucibus. In hac habitasse platea dictumst. Phasellus in blandit erat. Duis id facilisis magna.
-
-                Aenean vitae nisi quis mi ultricies finibus nec id massa. Sed ex nibh, consequat at magna ac, egestas volutpat ante. Sed in ante sit amet orci posuere congue. Sed id ultrices velit, vel convallis nunc. Quisque suscipit arcu consequat augue hendrerit, a mollis tellus cursus. Proin tristique nulla id vestibulum venenatis. Mauris ut luctus elit. Sed ut placerat ex. In hac habitasse platea dictumst. Nunc luctus nulla ex, non placerat orci viverra vitae. Phasellus vehicula imperdiet arcu, eu tristique nunc tempor at. Nam faucibus ex vitae augue maximus gravida. Aliquam vel mi orci. Nulla facilisi. Fusce neque urna, ultrices quis ex ac, tempus molestie sapien.</text>
         <h2 id="app" > App </h2>
             <text> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor quam eu posuere luctus. Quisque metus leo, consequat at euismod maximus, facilisis ut sem. Duis tincidunt felis efficitur lacus volutpat aliquet. Morbi suscipit, quam non commodo tempus, magna libero tincidunt mauris, nec tincidunt risus est in eros. Suspendisse iaculis laoreet magna id volutpat. Fusce eget nisl enim. Cras porttitor vehicula accumsan. Suspendisse velit purus, ultricies at risus ac, porttitor dapibus ex. Maecenas vel enim egestas, malesuada ligula eu, blandit mi. Phasellus tempus nisi eget erat mollis finibus. Vivamus a faucibus elit. In efficitur eget dui in placerat. Integer vestibulum dui in tortor tristique condimentum.
 
