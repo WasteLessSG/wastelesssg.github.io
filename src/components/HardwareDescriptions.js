1a09fe3548b523f5ccbcc247b7ee6bcd3e0b09e8
pmr99 <pradeep991004@gmail.com>
2020-12-30 00:41:19 +0800

    Added Hardware Folder

---
+++ src/components/HardwareDescriptions.js
@@ -0,0 +1,28 @@
+import leaderboard from './../assets/LeaderboardPage.png';
+import history from './../assets/HistoryPage.png';
+import statistics from './../assets/StatisticsPage.png';
+
+
+const leaderboardsection = {
+    headertext: "Community Leaderboard enables users to compare their performance with the rest of the community",
+    name : "Community Leaderboard",
+    elaboration: "The leaderboard section very nice one",
+    src: leaderboard
+};
+
+const historysection = {
+    headertext: "Seamlessly links waste data with the App when waste is disposed using our Smart Bin",
+    name : "History Page",
+    elaboration: "add bs ",
+    src: history
+};
+
+const statisticsection = {
+    headertext: "Provides access to individual waste disposal trends",
+    name : "Statistics Page",
+    elaboration: "more bs ",
+    src: statistics
+};
+
+
+export {leaderboardsection,historysection,statisticsection};
