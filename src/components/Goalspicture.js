3423d622353a7e6cfa66bc03a781c3b63d53916b
pmr99 <pradeep991004@gmail.com>
2020-12-30 22:29:44 +0800

    minor debugging

--- src/components/Goalspicture.js
+++ src/components/Goalspicture.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 
-const Hardwarepicture = (props) =>{
+const Goalspicture = (props) =>{
     return(
-        <div className="Hardwarepicture">
+        <div className="Goalspicture">
             <img src={props.src} className="rounded-circle" height={245} width={245} alt=""/>
             <h5 className="font-weight-bold mt-4" style={{color:'blue'}}>{props.name}</h5>
             <h5 className="font-weight-bold">{props.description}</h5>
@@ -11,4 +11,4 @@ const Hardwarepicture = (props) =>{
         </div>
     );
 }
-export default Hardwarepicture;
+export default Goalspicture;
