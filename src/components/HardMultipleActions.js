38594d0039a3be380754c797f53e5c4367e0759b
Pradeep Mani Rathnam Saravana Rathnam <pradeep991004@gmail.com>
2021-05-30 00:35:14 +0800

    final edits
    hovering feature and rectified scroll capabilities

--- src/components/HardMultipleActions.js
+++ src/components/HardMultipleActions.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 
 function HardMultipleActions() {
+  if (window.innerWidth<1000) {
+    window.location.replace("#/solution");
+    scroll.scrollTo(4000);
+  }
+  else {
   window.location.replace("#/solution");
   scroll.scrollTo(1850);
 }
+}
+
 export default HardMultipleActions;
--- src/components/MultipleActions.js
+++ src/components/MultipleActions.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 
 function AppMultipleActions() {
+if (window.innerWidth<1000) {
   window.location.replace("#/solution");
-  scroll.scrollTo(800);
+  scroll.scrollTo(1900);
+}
+else {
+window.location.replace("#/solution");
+scroll.scrollTo(800);
+}
 }
 
-function HardMultipleActions() {
-  window.location.replace("#/solution");
-  scroll.scrollTo(1200);
-}
 export default AppMultipleActions;
